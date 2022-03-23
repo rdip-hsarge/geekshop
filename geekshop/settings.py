76d3fb762f19e29ba565a2f743996af789d13e75
rdip-hsarge <86914866+rdip-hsarge@users.noreply.github.com>
2022-03-23 23:41:19 +0300

    Lesson 4 (#4)
    * Lesson 4. Data from files
    
    * Lesson 4. Auth app and user model
    
    * Lesson 4. User login and logout
    
    * Lesson 4. User CRUD

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "mainapp",
+    'authnapp',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Auth model
+AUTH_USER_MODEL = 'authnapp.ShopUser'
